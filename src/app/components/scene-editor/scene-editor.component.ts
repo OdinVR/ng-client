641e46d19fe273528685a8730c38148135d9c08f
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-31 21:11:54 +0530

    Create table for scene properties

--- src/app/components/scene-editor/scene-editor.component.ts
+++ src/app/components/scene-editor/scene-editor.component.ts
@@ -7,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SceneEditorComponent implements OnInit {
 
-  private spinAxis = 'x';
-
+  private spinAxis: string = 'x';
   private spinAxisOptions = [
     {value: 'x', viewValue: 'X'},
     {value: 'y', viewValue: 'Y'},
     {value: 'z', viewValue: 'Z'}
+  ];
+
+  private skyboxType: string = 'grid';
+  private skyboxTypeOptions = [
+    {value: 'grid', viewValue: 'Grid'},
+    {value: 'milky', viewValue: 'Galaxy'}
   ];
 
   constructor() { }
