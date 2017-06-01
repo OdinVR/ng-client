54b0aa2b5278fb98385e74785227f7962faf40c6
Vishnu Vijayan <vvijayan@purdue.edu>
2017-06-01 14:20:52 +0530

    Create properties for model and environment

--- src/app/components/scene-editor/scene-editor.component.ts
+++ src/app/components/scene-editor/scene-editor.component.ts
@@ -7,20 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SceneEditorComponent implements OnInit {
 
-  private spinAxis: string = 'x';
   private spinAxisOptions = [
     {value: 'x', viewValue: 'X'},
     {value: 'y', viewValue: 'Y'},
     {value: 'z', viewValue: 'Z'}
   ];
 
-  private skyboxType: string = 'grid';
   private skyboxTypeOptions = [
     {value: 'grid', viewValue: 'Grid'},
-    {value: 'milky', viewValue: 'Galaxy'}
+    {value: 'milky', viewValue: 'Space'}
   ];
 
-  constructor() { }
+  /* Model properties */
+  private xposition: number;
+  private yposition: number;
+  private zposition: number;
+  private scale: number;
+  private xrotation: number;
+  private yrotation: number;
+  private zrotation: number;
+  private spin: boolean;
+  private spinAxis: string;
+
+  /* Environment properties */
+  private skyboxType: string;
+  private skyboxSize: number;
+  private skyboxPosition: number;
+  private cameraHeight: number;
+
+  constructor() {
+    this.xposition = 0;
+    this.yposition = 0;
+    this.zposition = 0;
+    this.scale = 1;
+    this.xrotation = 0;
+    this.yrotation = 0;
+    this.zrotation = 0;
+    this.spin = false;
+    this.spinAxis = 'x';
+
+    this.skyboxType = 'grid'
+    this.skyboxSize = 50;
+    this.skyboxPosition = 25;
+    this.cameraHeight = 1.5;
+  }
 
   ngOnInit() {
   }
