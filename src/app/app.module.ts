678ff365c5a77042da92a37191a2b3a416045cd0
Vishnu Vijayan <vvijayan@purdue.edu>
2017-06-02 13:37:52 +0530

    Add tooltips to scene buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdIconModule, MdInputModule, MdSlideToggleModule, MdSelectModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdIconModule, MdInputModule, MdSlideToggleModule, MdSelectModule, MdTooltipModule } from '@angular/material';
 import { ViewportComponent } from './components/viewport/viewport.component';
 import { SceneEditorComponent } from './components/scene-editor/scene-editor.component';
 import { KeyframeEditorComponent } from './components/keyframe-editor/keyframe-editor.component';
@@ -32,7 +32,8 @@ import 'hammerjs';
     MdIconModule,
     MdInputModule,
     MdSlideToggleModule,
-    MdSelectModule
+    MdSelectModule,
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
